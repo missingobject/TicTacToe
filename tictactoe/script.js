2568c69d5c3c17340b02d98239b13e0e97759512
Giga <129690690+missingobject@users.noreply.github.com>
2023-12-30 17:48:57 +0400

    Update script.js

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -1,27 +1,23 @@
-// Constants
+`use strict`;
 const tiles = document.querySelectorAll(".tile");
 const firstPlayer = "X";
 const secondPlayer = "O";
 
-// Game state
 let turn = firstPlayer;
-let player1Score = 0;
-let player2Score = 0;
-const boardState = Array(tiles.length).fill(null);
+let firstPlayerScore = 0;
+let secondPlayerScore = 0;
+const boxState = Array(tiles.length).fill(null);
 
-// HTML elements
-const player1ScoreElement = document.getElementById("player1-score");
-const player2ScoreElement = document.getElementById("player2-score");
-const strike = document.getElementById("strike");
+const firstPlayerScoreElement = document.getElementById("first-player-score");
+const secondPlayerScoreElement = document.getElementById("second-player-score");
+const wincombo = document.getElementById("wincombo");
 const gameOverArea = document.getElementById("game-over-area");
 const gameOverText = document.getElementById("game-over-text");
 const playAgain = document.getElementById("play-again");
 
-// Event listeners
 playAgain.addEventListener("click", startNewGame);
 tiles.forEach((tile) => tile.addEventListener("click", tileClick));
 
-// Functions
 function setHoverText() {
   tiles.forEach((tile) => {
     tile.classList.remove("x-hover");
@@ -50,11 +46,11 @@ function tileClick(event) {
 
   if (turn === firstPlayer) {
     tile.innerText = firstPlayer;
-    boardState[tileNumber - 1] = firstPlayer;
+    boxState[tileNumber - 1] = firstPlayer;
     turn = secondPlayer;
   } else {
     tile.innerText = secondPlayer;
-    boardState[tileNumber - 1] = secondPlayer;
+    boxState[tileNumber - 1] = secondPlayer;
     turn = firstPlayer;
   }
 
@@ -64,19 +60,23 @@ function tileClick(event) {
 
 function checkWinner() {
   for (const winningCombination of winningCombinations) {
-    const { combo, strikeClass } = winningCombination;
-    const tileValue1 = boardState[combo[0] - 1];
-    const tileValue2 = boardState[combo[1] - 1];
-    const tileValue3 = boardState[combo[2] - 1];
+    const { combination, wincomboClass } = winningCombination;
+    const tileValue1 = boxState[combination[0] - 1];
+    const tileValue2 = boxState[combination[1] - 1];
+    const tileValue3 = boxState[combination[2] - 1];
 
-    if (tileValue1 != null && tileValue1 === tileValue2 && tileValue1 === tileValue3) {
-      strike.classList.add(strikeClass);
+    if (
+      tileValue1 != null &&
+      tileValue1 === tileValue2 &&
+      tileValue1 === tileValue3
+    ) {
+      wincombo.classList.add(wincomboClass);
       gameOverScreen(tileValue1);
       return;
     }
   }
 
-  const allTileFilledIn = boardState.every((tile) => tile !== null);
+  const allTileFilledIn = boxState.every((tile) => tile !== null);
   if (allTileFilledIn) {
     gameOverScreen(null);
   }
@@ -87,9 +87,9 @@ function gameOverScreen(winnerText) {
   if (winnerText != null) {
     text = `Winner is ${winnerText}!`;
     if (winnerText === firstPlayer) {
-      player1Score++;
+      firstPlayerScore++;
     } else if (winnerText === secondPlayer) {
-      player2Score++;
+      secondPlayerScore++;
     }
     updateScore();
   }
@@ -98,35 +98,34 @@ function gameOverScreen(winnerText) {
 }
 
 function updateScore() {
-  player1ScoreElement.innerText = `Player 1: ${player1Score}`;
-  player2ScoreElement.innerText = `Player 2: ${player2Score}`;
+  firstPlayerScoreElement.innerText = `Player 1: ${firstPlayerScore}`;
+  secondPlayerScoreElement.innerText = `Player 2: ${secondPlayerScore}`;
 }
 
 function startNewGame() {
-  strike.className = "strike";
+  wincombo.className = "wincombo";
   gameOverArea.classList.remove("visible");
-  boardState.fill(null);
+  boxState.fill(null);
   tiles.forEach((tile) => (tile.innerText = ""));
   turn = firstPlayer;
   setHoverText();
   updateScore();
 }
 
-// Winning combinations
 class WinningCombination {
-  constructor(combo, strikeClass) {
-    this.combo = combo;
-    this.strikeClass = strikeClass;
+  constructor(combination, wincomboClass) {
+    this.combination = combination;
+    this.wincomboClass = wincomboClass;
   }
 }
 
 const winningCombinations = [
-  new WinningCombination([1, 2, 3], "strike-row-1"),
-  new WinningCombination([4, 5, 6], "strike-row-2"),
-  new WinningCombination([7, 8, 9], "strike-row-3"),
-  new WinningCombination([1, 4, 7], "strike-column-1"),
-  new WinningCombination([2, 5, 8], "strike-column-2"),
-  new WinningCombination([3, 6, 9], "strike-column-3"),
-  new WinningCombination([1, 5, 9], "strike-diagonal-1"),
-  new WinningCombination([3, 5, 7], "strike-diagonal-2"),
+  new WinningCombination([1, 2, 3], "wincombo-row-1"),
+  new WinningCombination([4, 5, 6], "wincombo-row-2"),
+  new WinningCombination([7, 8, 9], "wincombo-row-3"),
+  new WinningCombination([1, 4, 7], "wincombo-column-1"),
+  new WinningCombination([2, 5, 8], "wincombo-column-2"),
+  new WinningCombination([3, 6, 9], "wincombo-column-3"),
+  new WinningCombination([1, 5, 9], "wincombo-diagonal-1"),
+  new WinningCombination([3, 5, 7], "wincombo-diagonal-2"),
 ];
